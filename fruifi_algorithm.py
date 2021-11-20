ad6678296af7f03289f57879949b0ce7f406db35
Muhammad Owais <owais.quest@gmail.com>
2021-11-20 16:40:01 +0500

    modifing

--- fruifi_algorithm.py
+++ fruifi_algorithm.py
@@ -1,79 +1,87 @@
-import pandas
-import re
+import csv
 
 
-#export csv file
-nd = pandas.read_csv(r'G:/Work/algorithm/aplpha.csv') #nd = nutrition details
+fn = []
+fd = []
+n = []
+good_for = []
+bad_for = []
 
 
-#convert column into lists
-fn = list(nd['Fruit name']) #fn = fruit name
-fd = list(nd['Description']) #fd = fruit description
-n = list(nd['Nutritions']) # n = Nutritions
-good_for = list(nd['Good for'])
-bad_for = list(nd['Bad for'])
-
-
-#making dictionary of csv file
-fruits = {}
-i = 0
-for fruit_name in fn:
-    fruits[fruit_name] = {
-        "description" : fd[i],
-        "nutrition" : n[i].split(","),
-        "good_for" : good_for[i].split(","),
-        "bad_for" : bad_for[i].split(",")
-    }
-    i = i + 1
-
-
-#give details of particular fruit
-def give_details(fruit_name):
-    print("Fruit Name: " + fruit_name)
-    print("Description: " + fruits[fruit_name]["description"])
-    
-    print("Nutritions:")
-    for nutrition in fruits[fruit_name]["nutrition"]:
-        print("- " + nutrition)
-    
-    print("Good For: ")
-    for good_for in fruits[fruit_name]["good_for"]:
-        print("- " + good_for)
-    
-    print("Bad For: ")
-    for bad_for in fruits[fruit_name]["bad_for"]:
-        print("- " + bad_for)
+with open('aplpha.csv', 'r') as csv_file:
+    try:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader, None) #skip first header line
         
-
-#check which fruit is good for particular fruit and which is not
-def check_good_bad(desise):
-    print("Good for " + desise)
-    for fruit_name in fruits:
-        if desise in fruits[fruit_name]["good_for"]:
-            print(fruit_name)
+        for row in csv_reader:
+            fn.append(row[1])
+            fd.append(row[2])
+            n.append(row[3])
+            good_for.append(row[4])
+            bad_for.append(row[5])
+        
+        #making dictionary of csv file
+        fruits = {}
+        i = 0
+        for fruit_name in fn:
+            fruits[fruit_name] = {
+                "description" : fd[i],
+                "nutrition" : n[i].split(","),
+                "good_for" : good_for[i].split(","),
+                "bad_for" : bad_for[i].split(",")
+            }
+            i = i + 1
+        
+        
+        #check which fruit is good for particular fruit and which is not
+        def check_good_bad(desise):
+            print("Good for " + desise)
+            for fruit_name in fruits:
+                if desise in fruits[fruit_name]["good_for"]:
+                    print(fruit_name)
     
-    print("\nBad for " + desise)
-    for fruit_name in fruits:
-        if desise in fruits[fruit_name]["bad_for"]:
-            print(fruit_name)
+            print("\nBad for " + desise)
+            for fruit_name in fruits:
+                if desise in fruits[fruit_name]["bad_for"]:
+                    print(fruit_name)
+        
+        #give details of particular fruit
+        def give_details(fruit_name):
+            print("Fruit Name: " + fruit_name)
+            print("Description: " + fruits[fruit_name]["description"])
+    
+            print("Nutritions:")
+            for nutrition in fruits[fruit_name]["nutrition"]:
+                print("- " + nutrition)
+    
+            print("Good For: ")
+            for good_for in fruits[fruit_name]["good_for"]:
+                print("- " + good_for)
+    
+            print("Bad For: ")
+            for bad_for in fruits[fruit_name]["bad_for"]:
+                print("- " + bad_for)
+ 
+ 
+ 
+        #program starts
+        print("1. Check Nutrition Details")
+        print("2. Check fruit is good or not for particular desise")
+        option = input("Enter number >> ")
+
+        if int(option) == 1:
+            print("Check for Fruits Neutritions")
+            i = 1
+            for fruit_name in fruits:
+                print(str(i) + ". " + fruit_name)
+                i += 1
 
 
-#program starts
-print("1. Check Nutrition Details")
-print("2. Check fruit is good or not for particular desise")
-option = input("Enter number >> ")
+            fruit_name = input("Type fruit name >> ")
+            give_details(fruit_name)
 
-if int(option) == 1:
-    print("Check for Fruits Neutritions")
-    i = 1
-    for fruit_name in fruits:
-        print(str(i) + ". " + fruit_name)
-        i += 1
-
-
-    fruit_name = input("Type fruit name >> ")
-    give_details(fruit_name)
-
-elif int(option) == 2:
-    desise = input("What you are feeling now >> ")
-    check_good_bad(desise)
+        elif int(option) == 2:
+            desise = input("What you are feeling now >> ")
+            check_good_bad(desise)
+    except:
+        print("File reading error")
--- test.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-nd = pd.read_csv("G:/Work/algorithm/FruitNutritionsDetails.csv")
-
